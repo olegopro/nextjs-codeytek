3cbf18cd5a7ca8b6055c7a04cf79a5abf0b260b0
olegopro <evilgazz@gmail.com>
2021-05-30 23:43:44 +0300

    Вывел массив меню через getStaticProps.

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,7 +1,23 @@
-export default function Index() {
+import client from '../src/apollo/client'
+import { GET_MENUS } from '../src/queries/get-menus'
+
+export default function Index({ menus }) {
+	console.log(menus)
 	return (
 		<div>
 			<h3 className="text-lg leading-6 font-medium text-gray-900">Applicant Information</h3>
 		</div>
 	)
+}
+
+export const getStaticProps = async ctx => {
+	const { data, loading, networkStatus } = await client.query({
+		query: GET_MENUS
+	})
+
+	return {
+		props: {
+			menus: { headerMenus: data?.headerMenus?.edges, footerMenus: data?.footerMenus?.edges }
+		}
+	}
 }
