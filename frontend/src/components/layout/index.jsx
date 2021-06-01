aa3e0ef4712f60c97002aec944d817487298d3a9
olegopro <evilgazz@gmail.com>
2021-06-01 15:41:27 +0300

    Сделал превью поста через админку.

--- frontend/src/components/layout/index.jsx
+++ frontend/src/components/layout/index.jsx
@@ -1,38 +1,53 @@
 import { isEmpty } from 'lodash'
 import Head from 'next/head'
+import PropTypes from 'prop-types'
 import { sanitize } from '../../utils/DOMPurify'
 import Seo from '../seo'
 import Footer from './footer'
 import Header from './header'
-// import Link from 'next/link'
 
-const Layout = ({ data, children }) => {
-	if (isEmpty(data?.page)) {
+const Layout = ({ data, isPost, children }) => {
+	const { page, post, header, footer, headerMenus, footerMenus } = data || {}
+
+	// If it does not have either post or page.
+	if (isEmpty(page) && isEmpty(post)) {
 		return null
 	}
 
-	const { page, header, footer, headerMenus, footerMenus } = data || {}
+	const seo = isPost ? post?.seo ?? {} : page?.seo ?? {}
+	const uri = isPost ? post?.uri ?? {} : page?.uri ?? {}
 
 	return (
 		<div>
-			<Seo seo={page?.seo} uri={page?.uri} />
+			<Seo seo={seo} uri={uri} />
 			<Head>
-				<link rel="shortcut icon" href={header.favicon} />
-				{page?.seo?.schemaDetails && (
+				<link rel="shortcut icon" href={header?.favicon} />
+				{seo?.schemaDetails ? (
 					<script
 						type="application/ld+json"
 						className="yoast-schema-graph"
 						key="yoastSchema"
-						dangerouslySetInnerHTML={{ __html: sanitize(page?.seo?.schemaDetails) }}
+						dangerouslySetInnerHTML={{ __html: sanitize(seo.schemaDetails) }}
 					/>
-				)}
+				) : null}
 			</Head>
-
 			<Header header={header} headerMenus={headerMenus?.edges} />
-			<div className="h-almost-screen">{children}</div>
+			<div className="container px-5 py-24 mx-auto">{children}</div>
 			<Footer footer={footer} footerMenus={footerMenus?.edges} />
 		</div>
 	)
+}
+
+Layout.propTypes = {
+	data: PropTypes.object,
+	isPost: PropTypes.bool,
+	children: PropTypes.object
+}
+
+Layout.defaultProps = {
+	data: {},
+	isPost: false,
+	children: {}
 }
 
 export default Layout
